1a130bf31c541f034f67be074e7135717f59d0d5
m3y <ma3ya.ozw+github@gmail.com>
2015-11-17 01:26:15 +0900

    exercise1

--- scala/src/main/scala/hatena/intern/Exercise1.scala
+++ scala/src/main/scala/hatena/intern/Exercise1.scala
@@ -1,9 +1,16 @@
 package hatena.intern
 
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.format._
+
 case class Log(host: String, user: String, epoch: Int, req: String, status: Int, size: Int, referer: String) {
-  def method: String = ???
-  def path: String = ???
-  def protocol: String = ???
-  def uri: String = ???
-  def time: String = ???
+  def method: String = req.split(" ")(0)
+  def path: String = req.split(" ")(1)
+  def protocol: String = req.split(" ")(2)
+  def uri: String = "http://%s%s".format(host, path)
+  def time: String = {
+    val dt = new DateTime(epoch * 1000L, DateTimeZone.UTC)
+    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss").print(dt)
+  }
 }
