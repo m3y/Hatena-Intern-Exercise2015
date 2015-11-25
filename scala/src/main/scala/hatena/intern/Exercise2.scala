0cc5eba36c2838427330a26a2a5ffa4fe7309cdb
m3y <ma3ya.ozw+github@gmail.com>
2015-11-25 16:07:15 +0900

    改行の追加

--- scala/src/main/scala/hatena/intern/Exercise2.scala
+++ scala/src/main/scala/hatena/intern/Exercise2.scala
@@ -15,7 +15,9 @@ object LtsvParser {
     // File -> String -> List[Map]
     val tokens = for {
       line <- Path.fromString(filePath).lines()
-      token = line.split("\t").map(x => x.split(":", 2)).map(pair => pair(0) -> pair(1)).toMap
+      token = line.split("\t")
+                  .map(x => x.split(":", 2))
+                  .map(pair => pair(0) -> pair(1)).toMap
     } yield token
 
     // List[Map] -> List[Log]
